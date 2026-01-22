fc713677ceb7191e58f744aec9666a34a4b4fea6
Mathis Lauriol-Torcq <mathis.lauriol-torcq@etu.univ-amu.fr>
2026-01-22 12:34:05 +0100

    Added the border color depending on the value

--- main.js
+++ main.js
@@ -16,12 +16,29 @@ fillArray(A_tableau);
 function changeValue(S_id, S_newValue) {
     const elem = document.getElementById(S_id);
     elem.innerHTML = S_newValue;
+    updateCss(S_newValue, elem);
 }
 
 
 function displayNumbers() {
     for (let I_index = 0 ; I_index < A_tableau.length ; I_index++){
 	    setTimeout(changeValue, I_index*2000, "js_value", A_tableau[I_index]);
+    }
+}
+
+function updateCss(S_value, O_elem) {
+    if (S_value > 30) {
+        console.log("30");
+        O_elem.setAttribute("class", "red");
+    } else if (S_value > 20) {
+        console.log("20");
+        O_elem.setAttribute("class", "orange");
+    }else if (S_value > 0) {
+        console.log("10");
+        O_elem.setAttribute("class", "green");
+    }else if (S_value > -10) {
+        console.log("-10");
+        O_elem.setAttribute("class", "blue");
     }
 }
 
